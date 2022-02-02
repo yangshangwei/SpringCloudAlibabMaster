f5c1d61b3eb48c573254cdc2e79514c8d2617444
yangshangwei <yswcomeon@gmail.com>
2022-02-02 17:45:07 +0800

    RestTemplate 实现自定义负载均衡算法 -- 增加服务端 通过端口识别请求地址

--- artisan-cloud-custom-lb-product/src/main/java/com/artisan/v1/controller/ProductInfoController.java
+++ artisan-cloud-custom-lb-product/src/main/java/com/artisan/v1/controller/ProductInfoController.java
@@ -2,7 +2,9 @@ package com.artisan.v1.controller;
 
 import com.artisan.common.entity.ProductInfo;
 import com.artisan.mapper.ProductInfoMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@Slf4j
 public class ProductInfoController {
 
+
+    @Value("${server.port}")
+    private  Integer port ;
 
     @Autowired
     private ProductInfoMapper productInfoMapper;
 
     @RequestMapping("/selectProductInfoById/{productNo}")
     public Object selectProductInfoById(@PathVariable("productNo") String productNo) {
+        log.info("{} 被请求了",port);
         ProductInfo productInfo = productInfoMapper.selectProductInfoById(productNo);
         return productInfo;
     }
