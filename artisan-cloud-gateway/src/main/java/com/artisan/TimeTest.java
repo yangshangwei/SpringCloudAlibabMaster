555a29731dadcd722d326856c821b884a1ecfad4
yangshangwei <yswcomeon@gmail.com>
2022-02-13 23:08:19 +0800

    The After Route Predicate Factory

---
+++ artisan-cloud-gateway/src/main/java/com/artisan/TimeTest.java
@@ -0,0 +1,18 @@
+package com.artisan;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author 小工匠
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/2/13 18:27
+ * @mark: show me the code , change the world
+ */
+public class TimeTest {
+
+    public static void main(String[] args) {
+        System.out.println(ZonedDateTime.now());
+    }
+}
+    
